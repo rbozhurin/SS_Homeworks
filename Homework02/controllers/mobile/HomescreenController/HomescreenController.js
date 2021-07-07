787994c880ac569ab2b39238333afe3069bb53d8
Rayko Bozhurin <rbozhurin@gmail.com>
2021-07-07 12:52:04 +0300

    connection

--- Homework02/controllers/mobile/HomescreenController/HomescreenController.js
+++ Homework02/controllers/mobile/HomescreenController/HomescreenController.js
@@ -1,4 +1,26 @@
 define({ 
-	
 
- });
+  onViewCreated: function() {
+    this.view.init = this.init;
+    this.view.preShow = this.preShow;
+  },
+
+  init: function() {
+    this.view.onClick = this.clicked;
+//     this.view.PhoneFlexContainer.
+  },
+
+  preShow: function() {
+    var contact = kony.store.getItem("contactDetails");
+    this.view.lblName.text = contact.name;
+    this.view.imgPerson.src = contact.src;
+    this.view.txtNumber.text = contact.phone;
+    this.view.txtHome.text = contact.email;
+  },
+
+  clicked: function(){
+    var ntf = new kony.mvc.Navigation("frmSegList");
+    ntf.navigate();
+  }
+
+});
