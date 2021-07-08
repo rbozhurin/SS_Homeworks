036c9185a2bbcbe26bf554887323d5dd1681d6fc
Rayko Bozhurin <rbozhurin@gmail.com>
2021-07-08 11:31:44 +0300

    add edit feature

--- Homework02/controllers/mobile/flxHomescreenController/flxHomescreenController.js
+++ Homework02/controllers/mobile/flxHomescreenController/flxHomescreenController.js
@@ -1,5 +1,5 @@
 define({ 
-
+	
   onViewCreated: function() {
     this.view.init = this.init;
     this.view.preShow = this.preShow;
@@ -7,6 +7,8 @@ define({
 
   init: function() {
     this.view.onClick = this.clicked;
+    this.isEditable = false;
+    
   },
 
   preShow: function() {
@@ -15,6 +17,24 @@ define({
     this.view.imgPerson.src = contact.src;
     this.view.txtNumber.text = contact.phone;
     this.view.txtHome.text = contact.email;
+    this.view.lblEdit.onTouchStart = this.editText;
+    
+    this.view.txtHome.setEnabled(false);
+    this.view.txtNumber.setEnabled(false);
+  },
+  
+  editText: function() {
+    this.isEditable = !this.isEditable;
+    
+    if (this.isEditable) {
+      this.view.lblEdit.text = "Done";
+      this.view.txtHome.setEnabled(true);
+      this.view.txtNumber.setEnabled(true);
+    } else {
+      this.view.lblEdit.text = "Edit";
+      this.view.txtHome.setEnabled(false);
+     this.view.txtNumber.setEnabled(false);
+    }
   },
 
   clicked: function(){
